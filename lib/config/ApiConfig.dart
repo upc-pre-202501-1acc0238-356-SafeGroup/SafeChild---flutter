3351046a7886e6a0f0c364edcc08caad00f73039
Andorla0 <84116120+Andorla0@users.noreply.github.com>
2025-07-11 12:26:58 -0500

    chore: add local backend var

--- lib/config/ApiConfig.dart
+++ lib/config/ApiConfig.dart
@@ -7,7 +7,8 @@ class ApiConfig {
   // Si usas emulador de Android
   // static const String baseUrl = 'http://10.0.2.2:8080/api/v1';
   // Para producción
-  static const String baseUrl = 'https://safechild-lastdeploy-a8ahd7ccc8b4cjb3.canadacentral-01.azurewebsites.net/api/v1';
+ // static const String baseUrl = 'https://safechild-lastdeploy-a8ahd7ccc8b4cjb3.canadacentral-01.azurewebsites.net/api/v1';
+  static const String baseUrl = 'http://192.168.18.21:8093/api/v1';
 
   // Authentication endpoints (IAM)
   static const String signUp = '$baseUrl/authentication/sign-up';
@@ -16,7 +17,6 @@ class ApiConfig {
 
   // Función para crear URLs de recursos
   static String resource(String path) => '$baseUrl$path';
-
 
 
   static final envBaseUrl  = '${dotenv.env['URL_BACKEND_PRODUCTION']}/api/v1';
