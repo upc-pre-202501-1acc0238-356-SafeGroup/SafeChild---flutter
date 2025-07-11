bebbd035dcb001bc3d90be26a2bcd4705a3b8b12
P1ero0VL <u20211a620@upc.edu.pe>
2025-07-11 17:24:03 -0500

    feat: add caregiver and schedule endpoint connections

--- lib/config/ApiConfig.dart
+++ lib/config/ApiConfig.dart
@@ -7,7 +7,6 @@ class ApiConfig {
   // Si usas emulador de Android
   // static const String baseUrl = 'http://10.0.2.2:8080/api/v1';
   // Para producción
- // static const String baseUrl = 'https://safechild-lastdeploy-a8ahd7ccc8b4cjb3.canadacentral-01.azurewebsites.net/api/v1';
   static const String baseUrl = 'http://192.168.18.21:8093/api/v1';
 
   // Authentication endpoints (IAM)
@@ -25,6 +24,12 @@ class ApiConfig {
 
 
   //static final String reservationsAPIUrl = '$envBaseUrl + /reservations';
+
+  // Caregiver endpoints
+  static const String getCaregivers = '$baseUrl/caregiver';
+
+  // Caregiver schedule endpoints
+  static const String getSchedule = '$baseUrl/schedules/caregiver';
 
 
 }
